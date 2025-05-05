94a2cb254c18343bbf5637797bdf9dde35a38516
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-05-05 23:13:08 +0200

    feat(auth): enforce stronger login and register request verification

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,32 +2,96 @@ import express from "express";
 
 const authRouter = express.Router();
 
+const isFalsy = (value) => !value;
+const isUndefined = (value) => value === undefined;
+const isNotString = (value) => typeof value !== "string";
+const isShorterEqualThan = (value, shorterLength) => value < shorterLength;
+const isLongerEqualThan = (value, higherLength) => value > higherLength;
+const isLowerEqualThan = (value, lowerNumber) => value < lowerNumber;
+const isHigherEqualThan = (value, higherNumber) => value < higherNumber;
+const isNotEqual = (value, equalNumber) => value != equalNumber;
+
+// TO-CONSIDER: Add typescript without hot reload to avoid build problem ?
+// TO-CONSIDEr: Add prettier for better formatting ?
+const validateStringProperty = (value, valueName, minLength, maxLength) => {
+  if (isUndefined(value)) {
+    return new Error(`Cannot process undefined ${valueName} property !`);
+  }
+
+  if (isNotString(value)) {
+    return new Error(`Cannot process non string ${valueName} property !`);
+  }
+
+  if (isShorterEqualThan(value.length, minLength)) {
+    return new Error(
+      `Cannot process ${valueName} property shorter than ${minLength} characters !`
+    );
+  }
+
+  if (isLongerEqualThan(value.length, maxLength)) {
+    return new Error(
+      `Cannot process ${valueName} property longer than ${maxLength} characters !`
+    );
+  }
+
+  return true;
+};
+
 authRouter.post("/login", (req, res, next) => {
   try {
-    const jsonBody = req.body;
-
-    if (jsonBody === undefined) {
+    // SECURITY: Verify JSON body
+    if (isFalsy(req.body)) {
       throw new Error(`Cannot process undefined request body !`);
     }
 
-    const jsonKeys = Object.keys(jsonBody).length;
-    if (jsonKeys != 3) {
+    if (isNotEqual(Object.keys(req.body).length, 3)) {
       throw new Error(`Cannot process request body of the wrong size !`);
     }
 
-    const jsonUsername = jsonBody.username;
-    if (jsonUsername === undefined) {
-      throw new Error(`Cannot process undefined username property !`);
+    // SECURITY: Verify username property
+    // TO-CONSIDER: Enforce no special characters in username, only letters, hyphens and numbers ?
+    const MIN_USERNAME_LENGTH = 5;
+    const MAX_USERNAME_LENGTH = 50;
+    const usernameError = validateStringProperty(
+      req.body.username,
+      "username",
+      MIN_USERNAME_LENGTH,
+      MAX_USERNAME_LENGTH
+    );
+
+    if (usernameError instanceof Error) {
+      throw usernameError;
     }
 
-    const jsonEmail = jsonBody.email;
-    if (jsonEmail === undefined) {
-      throw new Error(`Cannot process undefined email property !`);
+    // SECURITY: Verify email property
+    // TO-DO: Add email pattern verification
+    // TO-CONSIDER: Enforce no special characters in email, only letters, hyphens, @ and numbers ?
+    const MIN_EMAIL_LENGTH = 6;
+    const MAX_EMAIL_LENGTH = 150;
+    const emailError = validateStringProperty(
+      req.body.email,
+      "email",
+      MIN_EMAIL_LENGTH,
+      MAX_EMAIL_LENGTH
+    );
+
+    if (emailError instanceof Error) {
+      throw emailError;
     }
 
-    const jsonPassword = jsonBody.password;
-    if (jsonPassword === undefined) {
-      throw new Error(`Cannot process undefined password property !`);
+    // SECURITY: Verify password property
+    // TO-CONSIDER: Force stronger password with uppercase, lowercase, special characters and number verification ?
+    const MIN_PASSWORD_LENGTH = 6;
+    const MAX_PASSWORD_LENGTH = 200;
+    const passwordError = validateStringProperty(
+      req.body.password,
+      "password",
+      MIN_PASSWORD_LENGTH,
+      MAX_PASSWORD_LENGTH
+    );
+
+    if (passwordError instanceof Error) {
+      throw passwordError;
     }
 
     res.status(200).send(`Login route work fine`);
@@ -38,27 +102,47 @@ authRouter.post("/login", (req, res, next) => {
 
 authRouter.post("/register", (req, res, next) => {
   try {
-    const jsonBody = req.body;
-    if (jsonBody === undefined) {
+    // SECURITY: Verify JSON body
+    if (isFalsy(req.body)) {
       throw new Error(`Cannot process undefined request body !`);
     }
 
-    const jsonKeys = Object.keys(jsonBody).length;
-    if (jsonKeys != 2) {
+    if (isNotEqual(Object.keys(req.body).length, 2)) {
       throw new Error(`Cannot process request body of the wrong size !`);
     }
 
-    const jsonEmail = jsonBody.email;
-    if (jsonEmail === undefined) {
-      throw new Error(`Cannot process undefined email property !`);
+    // SECURITY: Verify email property
+    // TO-DO: Add email pattern verification
+    // TO-CONSIDER: Enforce no special characters in email, only letters, hyphens, @ and numbers ?
+    const MIN_EMAIL_LENGTH = 6;
+    const MAX_EMAIL_LENGTH = 150;
+    const emailError = validateStringProperty(
+      req.body.email,
+      "email",
+      MIN_EMAIL_LENGTH,
+      MAX_EMAIL_LENGTH
+    );
+
+    if (emailError instanceof Error) {
+      throw emailError;
     }
 
-    const jsonPassword = jsonBody.password;
-    if (jsonPassword === undefined) {
-      throw new Error(`Cannot process undefined password property !`);
+    // SECURITY: Verify password property
+    // TO-CONSIDER: Force stronger password with uppercase, lowercase, special characters and number verification ?
+    const MIN_PASSWORD_LENGTH = 6;
+    const MAX_PASSWORD_LENGTH = 200;
+    const passwordError = validateStringProperty(
+      req.body.password,
+      "password",
+      MIN_PASSWORD_LENGTH,
+      MAX_PASSWORD_LENGTH
+    );
+
+    if (passwordError instanceof Error) {
+      throw passwordError;
     }
 
-    res.status(200).send(`Register route work fine.`);
+    res.status(200).send(`Register route work fine`);
   } catch (error) {
     next(error);
   }
