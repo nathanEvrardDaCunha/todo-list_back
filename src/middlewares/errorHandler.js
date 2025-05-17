21add8cdec748f3cd18406c57847d450b494ffa3
nathanEvrardDaCunha <nathan.evrarddacunha@proton.me>
2025-05-17 12:46:57 +0200

    fix(core): update error handler to handle unexpected error properly

--- src/middlewares/errorHandler.js
+++ src/middlewares/errorHandler.js
@@ -1,12 +1,22 @@
 // Make it possible to send not only 500 but 400, 401, 402, 403, 404...
 // Make the filename, line and column work for more precise error information for debugging
 
+import { ClientError, ServerError } from '../utils/BaseError.js';
+
 function errorHandler(err, req, res, next) {
-    res.status(err.httpCode).json({
-        name: err.name,
-        cause: err.cause,
-        stack: err.stack,
-    });
+    if (err instanceof ClientError || err instanceof ServerError) {
+        res.status(err.httpCode).json({
+            name: err.name,
+            cause: err.cause,
+            stack: err.stack,
+        });
+    } else {
+        res.status(500).json({
+            name: 'External Error',
+            cause: err.message,
+            stack: err.stack,
+        });
+    }
 }
 
 export default errorHandler;
